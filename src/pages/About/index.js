7b1fbca9e03d544ec54740d234adf2984e3568cd
Jaelson <jaelsonjunior99@gmail.com>
2017-05-31 01:46:08 -0300

    Update pages

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Layout, Row, Col } from 'antd';
+const { Content } = Layout;
 
 // Require About component style
 require('./style.sass');
@@ -6,7 +8,19 @@ require('./style.sass');
 export default () => {
   return(
     <div id="about">
-      <h2 className="page-title">About Page</h2>
+      <Content style={{ padding: '0 50px' }}>
+        <Row>
+          <Col span={8} offset={8}>
+            <Row type="flex" justify="center">
+              <h1>
+                A simple project to fecthData from an api and show the informations as a sortable and filterable list.
+                Using cutting-edge tecnologies you can use the skeleton of the repository to make great applications.
+              </h1>
+              <h1 id="source">You can check the source code <a href="https://github.com/jcarva/select_title">here</a>.</h1>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
     </div>
   )
 };
--- src/pages/NoMatch/index.js
+++ src/pages/NoMatch/index.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import { Layout, Row, Col } from 'antd';
+const { Content } = Layout;
 
 // Require NoMatch component style
 require('./style.sass');
 
 export default () => {
   return(
-    <div id="no-match">
-      <h2 className="page-title">No Match</h2>
+    <div id="no-match" className="nb-error">
+      <Content style={{ padding: '0 50px' }}>
+        <Row>
+          <Col span={12} offset={6}>
+            <Row type="flex" justify="center">
+              <Col span={6}>
+                <div>
+                  <img src="https://a0.muscache.com/airbnb/static/error_pages/404-Airbnb_final-d652ff855b1335dd3eedc3baa8dc8b69.gif" width="313" height="428" alt="Girl has dropped her ice cream."/>
+                </div>
+              </Col>
+              <Col span={6} offset={4}>
+                <div className="error-code">404</div>
+                <h3 className="font-bold">We couldn't find the page..</h3>
+                <div className="error-desc">
+                  Sorry, but the page you are looking for was either not found or does not exist. <br/>
+                  Try refreshing the page or click on the navigation bar buttons.
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
     </div>
   )
 };
